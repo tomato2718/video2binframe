f1d3662da73eb65a0534243e914f84526a9b7c75
tomato2718 <qweasdzxc030@gmail.com>
2025-08-12 11:18:21 +0800

    Refactor: Use Option instead of ERR when EOF

--- src/video.rs
+++ src/video.rs
@@ -9,7 +9,6 @@ type ErrorMessage = &'static str;
 const ERR_OPEN_FILE: ErrorMessage = "Failed to open video";
 const ERR_FAILED_TO_FIND_VIDEO: ErrorMessage = "Failed to find video stream";
 const ERR_FAILED_TO_OPEN_DECODER: ErrorMessage = "Failed to open decoder";
-const ERR_FILE_EOF: ErrorMessage = "File EOF";
 const ERR_READING_FILE: ErrorMessage = "Error reading file";
 
 pub struct VideoCapture {
@@ -66,17 +65,17 @@ impl VideoCapture {
         Ok((index as i32, decoder))
     }
 
-    pub fn extract(&mut self) -> Result<AVPacket, &'static str> {
+    pub fn extract(&mut self) -> Result<Option<AVPacket>, &'static str> {
         loop {
             let packet = match self.input.read_packet() {
                 Ok(Some(p)) => p,
-                Ok(None) => return Err(ERR_FILE_EOF),
+                Ok(None) => return Ok(None),
                 Err(_) => return Err(ERR_READING_FILE),
             };
             if packet.stream_index != self.video_index {
                 continue;
             }
-            return Ok(AVPacket::from(packet));
+            return Ok(Some(AVPacket::from(packet)));
         }
     }
 
