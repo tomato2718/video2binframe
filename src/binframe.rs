7b3a553be5fcb811fb88f5e34eec25ca5e9a3b51
tomato2718 <qweasdzxc030@gmail.com>
2025-08-14 17:30:39 +0800

    Feat: Implement run length encode method

--- src/binframe.rs
+++ src/binframe.rs
@@ -16,6 +16,26 @@ impl BinFrame {
         self.buffer.extend(signature);
     }
 
+    pub fn write_run_length(&mut self, buffer: &[u8]) {
+        let mut run_length_buffer: Vec<u8> = Vec::new();
+        let mut last_pixel = 0xff;
+        for (current_index, current_pixel) in buffer.iter().enumerate() {
+            if current_pixel != &last_pixel {
+                run_length_buffer.extend_from_slice(&(current_index as u32).to_be_bytes());
+                last_pixel = *current_pixel;
+            }
+        }
+        run_length_buffer.extend_from_slice(&(buffer.len() as u32).to_be_bytes());
+        if run_length_buffer.len() > (u32::MAX >> 2) as usize {
+            panic!("Buffer size exceed");
+        }
+        let mut header_bytes = (run_length_buffer.len() as u32).to_be_bytes();
+        header_bytes[0] &= 0x3f;
+        header_bytes[0] |= 0x80;
+        self.buffer.extend(header_bytes);
+        self.buffer.extend(run_length_buffer);
+    }
+
     pub fn buffer(&self) -> &Vec<u8> {
         &self.buffer
     }
@@ -36,5 +56,23 @@ mod test {
         let signature_bytes = &bin_frame.buffer()[..24];
 
         assert_eq!(signature_bytes, want);
+    }
+
+    #[test]
+    fn write_run_length_given_buffer_should_write_to_frame_buffer() {
+        let mut bin_frame = BinFrame::new();
+
+        bin_frame.write_run_length(&[
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        ]);
+
+        assert_eq!(
+            bin_frame.buffer(),
+            &[
+                0x80, 0x00, 0x00, 0x0C, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00,
+                0x00, 0x18,
+            ]
+        );
     }
 }
